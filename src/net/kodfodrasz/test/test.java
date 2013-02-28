dcb85c57dbff7cd5cd61ed44582dcc3cbc905dcc
kódfodrász <ggergely@nohup.hu>
2013-02-28 08:33:56 +0100

    converted repositzor to eclipse java project
    - converted to eclipse java project
    - added sample main program class (test.java)

---
+++ src/net/kodfodrasz/test/test.java
@@ -0,0 +1,15 @@
+package net.kodfodrasz.test;
+
+public class test {
+
+	/**
+	 * Ez egy teszt alkalmazás a github és eclipse összekapcsolására.
+	 * 
+	 * @param args parancssori argumentumok
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ez egy teszt!");
+	}
+
+}
